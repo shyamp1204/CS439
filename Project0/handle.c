883a5c87bb54c652037f0178a4d3e649b1cdd846
Alex Irion <alex10@cs.utexas.edu>
2015-09-08 21:40:42 -0500

    1.1 in progress

--- Project0/handle.c
+++ Project0/handle.c
@@ -16,7 +16,10 @@
  *
  * Finally, loop forever, printing "Still here\n" once every
  * second.
+ *
+ *
  */
+void handler(int sig);
 int main(int argc, char **argv)
 {
   pid_t pid = getpid();
@@ -25,18 +28,47 @@ int main(int argc, char **argv)
   }
   else {
     printf("PID = %d\n", pid);
-    int time = 0;
     ssize_t bytes;
     const int STDOUT = 1;
-   
-    while(time <5) { // &&process still running
-      bytes = write(STDOUT, "Still here   \n", 10);
-      time++;
-      if(bytes != 10)
+    struct timespec timeWait;
+    struct timespec timeInterrupt;
+    timeWait.tv_sec = (time_t) 1;
+    timeWait.tv_nsec = 0;
+    int interrupt = 0;
+
+    while(1==1) {   //&&process still running
+      if(interrupt != -1) {
+	bytes = write(STDOUT, "Still here\n", 11);
+        timeWait.tv_sec = (time_t) 1;
+        timeWait.tv_nsec = 0;
+      }
+      if(bytes != 11)
 	exit(-999);
+      if(Signal(SIGINT, handler) == SIG_ERR)
+      	unix_error("signal error");
+      interrupt = nanosleep(&timeWait, &timeInterrupt);
+      if(interrupt == -1) {
+	timeWait.tv_nsec = timeInterrupt.tv_nsec;
+	timeWait.tv_sec = (time_t) 0;
+	interrupt = nanosleep(&timeWait, &timeInterrupt);
+      }
+      /*      if(interrupt ==0) {
+        timeWait.tv_sec = (time_t) 1;
+        timeWait.tv_nsec = 0;
+	}*/
     }
   }
   return 0;
+}
+
+//signal handler
+void handler(int sig) {
+  ssize_t bytes; 
+  const int STDOUT = 1; 
+
+  bytes = write(STDOUT, "Nice try.\n", 10); 
+  if(bytes != 10) 
+    exit(-999);
 }
 
 
