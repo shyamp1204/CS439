74adad7ea84ef6008daeeef7e4659dafbd4e8bdb
Katherine Heyne <kfh293@cs.utexas.edu>
2015-10-13 16:29:12 -0500

    Added comments to syscall.c for what to implement

--- PintOS/userprog/syscall.c
+++ PintOS/userprog/syscall.c
@@ -18,3 +18,21 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+
+/*
+THE SYSTEM CALLS (in lib/user/syscall.h):
+
+void halt (void);
+void exit (int status);
+pid_t exec (const char *file); //file is same as cmd_line
+int wait (pid_t pid);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned length);
+int write (int fd, const void *buffer, unsigned length);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
+*/
