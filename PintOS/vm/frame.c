b94fdab716776d473010632b720171c104fc2af5
Katherine Heyne <kfh293@cs.utexas.edu>
2015-11-09 13:21:17 -0600

    Added the functions to implement in frame.c and updated frame struct in frame.h

--- PintOS/vm/frame.c
+++ PintOS/vm/frame.c
@@ -1,5 +1,14 @@
 #include "vm/frame.h"
+#include "vm/page.h"
+#include "vm/swap.h"
+#include "threads/thread.h"
+#include "threads/synch.h"
+#include "threads/palloc.h"
+#include "threads/malloc.h"
+#include "userprog/pagedir.h"
+#include "threads/vaddr.h"
 #include <stdio.h>
+//THERE MIGHT BE MORE, BUT THESE ARE THE ONES I AM GUESSING WE WILL NEED TO INCLUDE!
 
 
 static unsigned hash_func(const struct hash_elem *e, void *aux);
@@ -18,6 +27,57 @@ frame_init()
 	lock_init(&frame_lock);
 	hash_init(&hash_frames, hash_func, less_func, NULL);
 }
+
+/*get a free frame (call palloc_get_page) and allocate this frame;
+if no frame available, evict a frame and use that one! */
+void*
+get_frame(enum palloc_flags flags)
+{
+	//this "addr" variable will be the return value at the end!
+	void* addr = palloc_get_page(flags);
+	//check if palloc was successful or not
+	if(addr != NULL) {
+		//successful memory allocation!
+	}
+	else {
+		//evict a frame and use this new frame (called evict function)
+		addr = evict_frame();
+	}
+
+	return addr;
+}
+
+
+//remove given frame (addr points to this frame) from the frame table and free its resources
+void
+free_frame(void* frame_addr)
+{
+
+}
+
+
+/*choose a frame to evict (using replacement algorithm),
+then clear it from the page directory of its "owner" thread*/
+void*
+evict_frame(void)
+{
+
+}
+
+//maps the given frame to a page from the user pool
+void
+frame_map(void* frame_addr, void* page_addr, bool writable)
+{
+
+}
+
+//unmaps the given frame
+void
+frame_unmap(void* frame_addr)
+{
+
+}
+
 
 // hash_hash_func function -- hash function for the hash table
 static unsigned
--- PintOS/vm/frame.h
+++ PintOS/vm/frame.h
@@ -5,9 +5,38 @@
 
 struct frame
 {
-	void* addr;  //PHYSICAL ADDRESS OF FRAME
-	void* page;  //POINTER TO THE PAGE WITHIN THIS FRAME
+	void* frame_addr;  //PHYSICAL ADDRESS OF FRAME
+	void* page;  //POINTER TO THE PAGE MAPPED TO THIS FRAME
 	struct thread* thread;  //THREAD THAT OWNS THIS FRAME
-	bool pinned;  //TRUE IS THE FRAME IS "PINNED"
-	struct hash_elem hash_elem;  //HASH ELEMENT FOR IMPLEMENTING HASH TABLE FOR FRAME TABLE
-}
+	struct hash_elem hash_elem;  //HASH ELEMENT FOR IMPLEMENTING HASH TABLE to store this frame in the frame table
+	// bool pinned;  //TRUE IS THE FRAME IS "PINNED" -- may not need this!
+};
+
+void
+frame_init();
+
+static unsigned
+hash_func(const struct hash_elem *e, void *aux);
+
+static bool
+less_func(const struct hash_elem *a, const struct hash_elem *b, void *aux);
+
+//get a free frame by calling palloc_get_page and allocate a frame; if no frame available, evict a frame and use that
+void*
+get_frame(enum palloc_flags flags);
+
+//remove a frame from the frame table and free it
+void
+free_frame(void* page_vaddr, uint32_t* pagedir);
+
+//choose a frame to evict and clear it from the page directory of its owner thread
+void*
+evict_frame();
+
+//maps the given frame to a page from the user pool
+void
+frame_map(void* frame_addr, void* page_addr, bool writable);
+
+//unmaps the given frame
+void
+frame_unmap(void* frame_addr);
