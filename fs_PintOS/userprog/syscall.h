228ba828510dc68651fdee4fbc5f3a80eb5c9fe3
Alex Irion <alex10@cs.utexas.edu>
2015-11-17 22:31:53 -0600

    begin filesys

--- fs_PintOS/userprog/syscall.h
+++ fs_PintOS/userprog/syscall.h
@@ -3,5 +3,10 @@
 
 void syscall_init (void);
 void exit_status_ext (int e_status);
+bool chdir (const char *dir);
+bool mkdir (const char *dir);
+bool readdir (int fd, char *name);
+bool isdir (int fd);
+int inumber (int fd);
 
 #endif /* userprog/syscall.h */
